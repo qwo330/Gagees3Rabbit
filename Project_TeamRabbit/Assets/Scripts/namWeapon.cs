3bc416cf47837b2203c117df8095c9d1436017e6
NamDaeHyeon <yumsoya@gmail.com>
2019-10-19 22:40:59 +0900

    플레이어컨트롤러 스크립트 추가. 무기컨트롤러 샘플 제작.

--- Project_TeamRabbit/Assets/Scripts/namWeapon.cs
+++ Project_TeamRabbit/Assets/Scripts/namWeapon.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class namWeapon : MonoBehaviour
 {
     [SerializeField] namPlayerController player;
+    [SerializeField] Transform weaponDir;
 
     void Start()
     {
@@ -15,9 +16,38 @@ public class namWeapon : MonoBehaviour
     {
         while (true)
         {
+            Vector3 mouseScreenPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
+            Vector3 lookAt = mouseScreenPosition;
+
+            float AngleRad = Mathf.Atan2(lookAt.y - transform.position.y, lookAt.x - transform.position.x);
+
+            float AngleDeg = (180 / Mathf.PI) * AngleRad;
+
+            transform.rotation = Quaternion.Euler(0, 0, AngleDeg);
+
+            if(!(transform.eulerAngles.z < 90 && transform.eulerAngles.z > -90))
+            {
+                weaponDir.localEulerAngles = new Vector3(0, 0, 180);
+                weaponDir.localScale = new Vector3(-1, 1, 1);
+            }
+            else
+            {
+                weaponDir.localEulerAngles = new Vector3(0, 0, 0);
+                weaponDir.localScale = new Vector3(1, 1, 1);
+            }
+
+            if(Input.GetMouseButtonDown(0))
+            {
+                Fire();
+            }
 
             yield return null;
         }
+    }
+
+    void Fire()
+    {
+        
     }
 }
