2f8aa1a35fc2fe9a18bd142ff28c025be68e5a59
qwo330 <qwd330@naver.com>
2019-10-20 06:47:51 +0900

    타이틀 씬 추가

---
+++ Project_TeamRabbit/Assets/Scripts/IntroScene.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class IntroScene : MonoBehaviour
+{
+    public void OnBtnStart()
+    {
+        SceneManager.LoadScene("Stage_Forest");
+    }
+
+    public void OnBtnExit()
+    {
+        Application.Quit();
+    }
+}
