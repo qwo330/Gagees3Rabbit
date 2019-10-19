d98e4b0a2ff0cfcd4fedc525b1898652e2c6c6b7
qwo330 <qwd330@naver.com>
2019-10-19 21:12:13 +0900

    프로젝트 세팅 수정 및 폴더구조 수정

---
+++ Project_TeamRabbit/Assets/Scripts/ObjectPool.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPool : MonoBehaviour
+{
+    static ObjectPool instance;
+    public static ObjectPool Get
+    {
+        get
+        {
+            if (instance == null)
+                instance = new ObjectPool();
+            return instance;
+        }
+    }
+
+
+}
