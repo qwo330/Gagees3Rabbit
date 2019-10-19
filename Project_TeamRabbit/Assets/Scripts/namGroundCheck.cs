7ef770f344b87f8239cff8968ab9febc351595ad
NamDaeHyeon <yumsoya@gmail.com>
2019-10-19 21:57:35 +0900

    플레이어 기능 추가 : 좌우 이동 및 점프.

---
+++ Project_TeamRabbit/Assets/Scripts/namGroundCheck.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class namGroundCheck : MonoBehaviour
+{
+    public bool isGround = false;
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if(collision.gameObject.layer.Equals(LayerMask.NameToLayer("Ground")))
+        {
+            isGround = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer.Equals(LayerMask.NameToLayer("Ground")))
+        {
+            isGround = false;
+        }
+    }
+}
