7d1d10075cf47bc07e4d6e80e28d71d7892b2411
qwo330 <qwd330@naver.com>
2019-10-20 09:03:02 +0900

    카메라 이동 추가

---
+++ Project_TeamRabbit/Assets/Scripts/CameraController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField]
+    Vector3 offset = new Vector3(25.0f, 12.0f, -86.4f);
+
+    void LateUpdate()
+    {
+        Vector3 pos = GameManager.Get.player.transform.position;
+        transform.position = new Vector3(pos.x, 0, pos.z) + offset;
+    }
+}
